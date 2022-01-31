90c1fd27b5113cb3ab25641db8ae7bcba5c4604a
AdalbertCosta <contato@adalbertocosta.com>
2022-01-31 07:04:07 -0300

    utilizando useState

--- react-dio/src/components/Card/index.js
+++ react-dio/src/components/Card/index.js
@@ -1,7 +1,28 @@
+import{ useState} from 'react';
+
+
 const Card = () => {
-    return (
-        <h1>Card</h1>
-    )
+
+  const [valor, setValor] = useState(0)
+
+  function Adicionar(){
+    setValor(valor + 1);
+  }
+
+  function Remover(){
+    setValor(valor - 1);
+  }
+
+  return (
+    <div  classeName="card">
+      <div classeName="card-header">Meu primeiro card</div>
+      <div classeName="card-body">
+        <button type="button" className="btn btn-success" onClick={Adicionar}>Adicionar</button>
+        <button type="button" className="btn btn-danger"  onClick={Remover}>Remover</button>
+        <p>{valor}</p>
+      </div>
+    </div>
+  )
 }
 
-export default Card;
+export default Card
